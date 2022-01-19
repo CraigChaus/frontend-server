0930ddd2ccf8ed1d479a907b909117fceb76ab24
CraigChaus <craigchauraya@gmail.com>
2022-01-19 19:40:31 +0100

    Implemented diffie hellman

---
+++ level_1_server/src/ENCRYPTION/ValueCalculator.java
@@ -0,0 +1,12 @@
+package ENCRYPTION;
+
+public class ValueCalculator {
+
+    public static long power(long a, long b, long p)
+    {
+        if (b == 1)
+            return a;
+        else
+            return (((long)Math.pow(a, b)) % p);
+    }
+}
